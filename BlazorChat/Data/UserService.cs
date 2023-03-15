8f2fa10a12a303c958d23caa3fadedeb0782fed6
Filip Copija <filip.copija1@gmail.com>
2023-03-15 16:51:52 +0100

    Add UserService.cs

---
+++ BlazorChat/Data/UserService.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorChat.Data
+{
+    public class UserService
+    {
+        private Dictionary<string, string> _users = new Dictionary<string, string>();
+
+        public void Add(string connectionId, string username)
+        {
+            _users.Add(connectionId, username);
+        }
+
+        public void RemoveByName(string username)
+        {
+            _users.Remove(username);
+        }
+
+        public string GetConnectionIdByName(string username)
+        {
+            return _users.TryGetValue(username, out var value) ? value : string.Empty;
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+            return _users.Select(e => (e.Value, e.Key));
+        }
+    }
+}
